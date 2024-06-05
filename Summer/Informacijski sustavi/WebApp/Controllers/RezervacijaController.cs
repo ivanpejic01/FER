c64ad2f852cefc2bd18db631007da43534205439
ivanpejic01 <ivanpejic72@gmail.com>
2024-06-05 16:10:11 +0200

    Web aplikacija i testovi iz infsusa

---
+++ Summer/Informacijski sustavi/WebApp/Controllers/RezervacijaController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class RezervacijaController : Controller
+    {
+        private readonly infsusContext ctx;
+        private readonly ILogger<PutovanjeController> logger;
+
+        public RezervacijaController(infsusContext ctx, ILogger<PutovanjeController> logger)
+        {
+            this.ctx=ctx;
+            this.logger=logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var rezervacija = await ctx.Rezervacija.FindAsync(id);
+
+            if (rezervacija == null)
+            {
+                return NotFound();
+            }
+
+            return View(rezervacija);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRezervacija,IdKorisnik,Potvrdeno,IdPutovanje,BrojMjesta,DatumRezervacije")] Rezervacija rezervacija)
+        {
+
+            if (!RezervacijaExists(rezervacija.IdRezervacija))
+            {
+                return View(rezervacija);
+            }
+            else
+            {
+                var postojecaRezervacija = await ctx.Rezervacija.FindAsync(id);
+                postojecaRezervacija.Potvrdeno = rezervacija.Potvrdeno;
+                ctx.Update(postojecaRezervacija);
+                await ctx.SaveChangesAsync();
+                return RedirectToAction("Details", "Putovanje", new { id = rezervacija.IdPutovanje });
+            }
+
+
+
+        }
+
+        private bool RezervacijaExists(int id)
+        {
+            return ctx.Rezervacija.Any(r => r.IdRezervacija == id);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var rezervacija = await ctx.Rezervacija.FindAsync(id);
+
+            if (rezervacija == null)
+            {
+                return NotFound();
+            }
+            var idPutovanja = rezervacija.IdPutovanje;
+            ctx.Rezervacija.Remove(rezervacija);
+            await ctx.SaveChangesAsync();
+            return RedirectToAction("Details", "Putovanje", new { id = idPutovanja });
+        }
+
+        public async Task<IActionResult> Create (int id)
+        {
+
+            var usersWithoutReservation = ctx.Korisnik
+            .Where(k => !ctx.Rezervacija.Any(r => r.IdPutovanje == id && r.IdKorisnik == k.IdKorisnik))
+            .ToList();
+
+            ViewData["IdKorisnik"] = new SelectList(usersWithoutReservation, "IdKorisnik", "KorisnickoIme");
+
+            var rezervacija = new Rezervacija
+            {
+                IdPutovanje = id,
+                DatumRezervacije = DateTime.Today
+            };
+
+            return View(rezervacija);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create (int idPutovanje, [Bind("IdKorisnik,IdPutovanje,DatumRezervacije,BrojMjesta")] Rezervacija rezervacija)
+        {
+
+            if (ModelState.IsValid)
+            {
+                rezervacija.DatumRezervacije = DateTime.Now;
+
+                ctx.Add(rezervacija);
+                ctx.SaveChanges();
+                return RedirectToAction("Details", "Putovanje", new { id = rezervacija.IdPutovanje });
+            }
+
+            var usersWithoutReservation = ctx.Korisnik
+            .Where(k => !ctx.Rezervacija.Any(r => r.IdPutovanje == idPutovanje && r.IdKorisnik == k.IdKorisnik))
+            .ToList();
+
+            ViewData["IdKorisnik"] = new SelectList(usersWithoutReservation, "IdKorisnik", "KorisnickoIme");
+
+            var rezervacijaReturn = new Rezervacija
+            {
+                IdPutovanje = idPutovanje,
+                DatumRezervacije = DateTime.Today
+            };
+
+            return View(rezervacijaReturn);
+        }
+    }
+
+}
---
+++ Summer/Informacijski sustavi/WebApp/ModelsValidation/MjestoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApp.ModelsValidation
+{
+    public class MjestoValidator : AbstractValidator<Mjesto>
+    {
+
+        public MjestoValidator()
+        {
+            RuleFor(d => d.Naziv)
+                .NotEmpty().WithMessage("Naziv mjesta je obavezno polje!")
+                .MaximumLength(50).WithMessage("Naziv mjesta ne može biti dulji od 50 znakova!");
+
+            RuleFor(d => d.Postanskibroj)
+                .NotEmpty().WithMessage("Poštanski broj je obavezno polje!")
+                .GreaterThan(0).WithMessage("Poštanski broj mora biti pozitivan")
+                .Must(w => w.ToString().Length == 5).WithMessage("Poštanski broj mora sadržavati točno 5 znamenki!");
+        }
+
+
+
+    }
+
+}
